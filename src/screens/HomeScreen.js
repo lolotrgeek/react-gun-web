8cc4955e929a04837698192894f02c9b0f1f3455
lolotrgeek <lolotrgeek@gmail.com>
2020-02-20 21:55:26 -0600

    timers and projects listed and ordered

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,6 +7,7 @@ import {
   useParams
 } from "react-router-dom"
 import { newProject, newTimer } from '../constants/Models'
+import { trimSoul } from '../constants/Store'
 
 import Gun from 'gun/gun'
 
@@ -45,6 +46,8 @@ export default function HomeScreen() {
           <Route path="/timers">
             <Timers />
           </Route>
+          <Route path="/timer/:id" children={<TimersChild />} />
+          <Route path="/project/:id" children={<ProjectsChild />} />
         </Switch>
       </div>
     </Router >
@@ -79,25 +82,19 @@ function Projects() {
   const [online, setOnline] = useState(false)
   const [projects, setProjects] = useState([])
 
-  // useEffect(() => {
-  //   gun.get('projects').once((data, key) => setOnline(true))
-  //   console.log()
-  //   return () => gun.get('projects').off()
-  // }, []);
-
   useEffect(() => {
     gun.get('projects').map().on((data, key) => setProjects(projects => [...projects, [key, data]])
       , { change: true })
     return () => gun.get('projects').off()
-  }, [online]);
+  }, [online])
 
   return (
     <div>
       <h2>Projects</h2>
       <div>Value: <ul>
-        {projects.map(item => {
+        {projects.map(project => {
           return (
-            <li>{item[0]}</li>
+            <li><Link to={`/project/${project[0]}`}>{project[0]}</Link></li>
           )
         })}
       </ul></div>
@@ -105,12 +102,91 @@ function Projects() {
   )
 }
 
+
 function Timers() {
+  const [online, setOnline] = useState(false)
+  const [timers, setTimers] = useState([])
+
+  useEffect(() => {
+    gun.get('timers').map().on((timerId, projectKey) => {
+      gun.get('timers').get(projectKey).map().on((timerId, timerKey) => {
+        gun.get('timers').get(projectKey).get(timerKey).map().on((timerValue, timerGunId) => {
+          console.log(timerValue)
+          setTimers(timers => [...timers, [timerKey, trimSoul(timerValue)]])
+        })
+      })
+    }
+      , { change: true })
+    return () => gun.get('timers').off()
+  }, [online]);
 
   return (
     <div>
       <h2>Timers</h2>
+      <div>
+        <ol>
+          {timers.map(timer => {
+            return (
+              <li><Link to={`/timer/${timer[0]}`}>{`${JSON.stringify(timer[1])}`}</Link></li>
+              // <li><Link to={`/timer/${timer[0]}`}>{`${timer[1].status}, ${timer[1].created}`}</Link></li>
+            )
+          })}
+        </ol></div>
+    </div >
+  )
+}
 
+function ProjectsChild() {
+  const { id } = useParams()
+  const [online, setOnline] = useState(false)
+  const [timers, setTimers] = useState([])
+
+  const createTimer = () => {
+    const timer = newTimer({ project: id })
+    gun.get('timers').get(id).get(timer[0]).set(timer[1])
+    // setTimers(timers => [...timers, timer])
+  }
+
+  useEffect(() => {
+    gun.get('timers').get(id).map().once((timerId, timerKey) => {
+      gun.get('timers').get(id).get(timerKey).map().on((timerValue, timerGunId) => {
+        console.log(timerValue)
+        setTimers(timers => [...timers, [timerKey, trimSoul(timerValue)]])
+      })
+    }
+      , { change: true })
+    return () => gun.get('timers').off()
+  }, [online]);
+
+
+  return (
+    <div>
+      <h2>Project {id}</h2>
+      <button type='button' onClick={() => createTimer()}>New Timer</button>
+      <h3>Timers</h3>
+      <div>
+        <ol>
+          {timers.map(timer => {
+            return (
+              <li><Link to={`/timer/${timer[0]}`}>{`${JSON.stringify(timer[1])}`}</Link></li>
+              // <li><Link to={`/timer/${timer[0]}`}>{`${timer[1].status}, ${timer[1].created}`}</Link></li>
+            )
+          })}
+        </ol></div>
+    </div >
+  )
+}
+
+function TimersChild() {
+  // We can use the `useParams` hook here to access
+  // the dynamic pieces of the URL.
+
+  return (
+    <div>
+      <h2>Timer </h2>
+      <div>
+
+      </div>
     </div>
   )
 }
