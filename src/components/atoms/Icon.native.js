c7f14a6c1a99571141b836ba52246c88f7281fe5
lolotrgeek <lolotrgeek@gmail.com>
2020-04-15 07:26:46 -0500

    no icon children

--- src/components/atoms/Icon.native.js
+++ src/components/atoms/Icon.native.js
@@ -7,18 +7,18 @@ import Icon from 'react-native-vector-icons/FontAwesome5'
  * @param {string} props.color 
  * @param {number} props.size 
  */
-export const TitleIcon = props => <Icon name='circle' size={props.size} color={props.color} children={props.children}  />
+export const TitleIcon = props => <Icon name='circle' size={props.size} color={props.color}  />
 
 /**
  * @param {*} props 
  * @param {string} props.color 
  * @param {number} props.size 
  */
-export const DrawerIcon = props => <Icon name='chevron-left' size={props.size} color={props.color} children={props.children}  />
+export const DrawerIcon = props => <Icon name='chevron-left' size={props.size} color={props.color}  />
 
 /**
  * @param {*} props 
  * @param {string} props.color 
  * @param {number} props.size 
  */
-export const MenuIcon = props => <Icon name='more_vert' size={props.size} color={props.color} children={props.children}  />
+export const MenuIcon = props => <Icon name='more_vert' size={props.size} color={props.color}  />
