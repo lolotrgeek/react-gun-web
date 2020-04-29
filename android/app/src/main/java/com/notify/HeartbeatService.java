7afee7e2c84533b6ae1cc9bf36500214368f3c40
lolotrgeek <lolotrgeek@gmail.com>
2020-04-29 08:05:36 -0500

    service modules for multiple long-running tasks

--- android/app/src/main/java/com/notify/HeartbeatService.java
+++ android/app/src/main/java/com/notify/HeartbeatService.java
@@ -11,6 +11,7 @@ import androidx.core.app.NotificationCompat;
 import android.app.NotificationManager;
 import android.app.NotificationChannel;
 import android.os.Build;
+import android.os.Message;
 
 import com.facebook.react.HeadlessJsTaskService;
 
@@ -22,7 +23,9 @@ public class HeartbeatService extends Service {
     private static HeartbeatService instance;
     // private int CURRENT_TICK = 0;
 
-    private Handler handler = new Handler();
+    private Handler countHandler = new Handler();
+    private Handler msgHandler = new Handler();
+    private Handler dataHandler = new Handler();
     private Runnable runnableCode = new Runnable() {
         @Override
         public void run() {
@@ -31,8 +34,17 @@ public class HeartbeatService extends Service {
             context.startService(myIntent);
             HeadlessJsTaskService.acquireWakeLockNow(context);
             // Here is the 'actual' logic of the service
-            // handler.postDelayed(this, INTERVAL);
-            // handler.post(this);
+            countHandler.postDelayed(this, INTERVAL);
+        }
+    };
+    private Runnable runnableDataCode = new Runnable() {
+        @Override
+        public void run() {
+            Context context = getApplicationContext();
+            Intent myIntent = new Intent(context, DataEventService.class);
+            context.startService(myIntent);
+            HeadlessJsTaskService.acquireWakeLockNow(context);
+            // Here is the 'actual' logic of the service
         }
     };
 
@@ -40,20 +52,28 @@ public class HeartbeatService extends Service {
         return instance;
     }
 
-    public void setRunnableInterval (int ms) {
-        INTERVAL = ms; 
+    public void setRunnableInterval(int ms) {
+        INTERVAL = ms;
     }
 
-    // resumes the service, use carefully, can cause service to step on itself
-    public void resume () {
-        this.handler.post(this.runnableCode);
+    // resumes the countHandler, use carefully, can cause service to step on itself
+    // TODO: make this a closure
+    public void resume() {
+        this.countHandler.post(this.runnableCode);
     }
 
-    // suspends the service, use carefully, can cause service to step on itself
-    public void pause () {
-        this.handler.removeCallbacks(this.runnableCode);
+    // suspends the countHandler, use carefully, can cause service to step on itself
+    // TODO: make this a closure
+    public void pause() {
+        this.countHandler.removeCallbacks(this.runnableCode);
     }
 
+    public void postInt(int num) {
+        Message message = new Message();
+        message.what = SERVICE_NOTIFICATION_ID;
+        message.arg1 = num;
+        this.msgHandler.sendMessage(message);
+    }
 
     private void createNotificationChannel() {
         // Create the NotificationChannel, but only on API 26+ because
@@ -83,27 +103,27 @@ public class HeartbeatService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        this.handler.removeCallbacks(this.runnableCode);
+        this.countHandler.removeCallbacks(this.runnableCode);
+        this.dataHandler.removeCallbacks(this.runnableDataCode);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // this.handler.post(this.runnableCode);
-        this.runnableCode.run();
+        this.dataHandler.post(this.runnableDataCode);
         createNotificationChannel();
         Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,PendingIntent.FLAG_CANCEL_CURRENT);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
         String title = intent.getStringExtra("TITLE");
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle(title)
-                .setContentText("Running...")
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentIntent(contentIntent)
-                .setOnlyAlertOnce(true)
-                .setPriority(NotificationCompat.PRIORITY_LOW)
-                .setOngoing(true)
-                .build();
+            .setContentTitle(title)
+            .setContentText("Ready...")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentIntent(contentIntent)
+            .setOnlyAlertOnce(true)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setOngoing(true)
+            .build();
         startForeground(SERVICE_NOTIFICATION_ID, notification);
         return START_STICKY;
     }
